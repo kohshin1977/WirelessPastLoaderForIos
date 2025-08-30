77f8366c294b92aba53fb92f8ef255ce873c8b91
kohshin1977 <onsight.system@gmail.com>
2025-08-30 21:03:45 +0900

    初回コミット

---
+++ WirelessPastLoaderForIos/WirelessPastLoaderForIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WirelessPastLoaderForIos
+//
+//  Created by 徳永功伸 on 2025/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WirelessPastLoaderForIos/WirelessPastLoaderForIos/WirelessPastLoaderForIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WirelessPastLoaderForIosApp.swift
+//  WirelessPastLoaderForIos
+//
+//  Created by 徳永功伸 on 2025/08/30.
+//
+
+import SwiftUI
+
+@main
+struct WirelessPastLoaderForIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
